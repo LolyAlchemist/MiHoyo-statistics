bb87480ddf1dd2355a55501a4653a4b34885cdad
Lavl <148311044+LolyAlchemist@users.noreply.github.com>
2025-02-22 15:17:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
 import flask
-from flask_peewee.db import Database
-from flask_peewee.auth import Auth
-from peewee import TextField, IntegerField, FloatField
 
-from werkzeug.utils import secure_filename
-
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, EmailField, FloatField as FF
-from wtforms.validators import DataRequired, Length, EqualTo
-
-DATABASE = {
-    'name': 'mihoyo.db',
-    'engine': 'peewee.SqliteDatabase'
-}
-app = flask.Flask(__name__) 
-app.config.from_object(__name__)
-
-db = Database(app)
-auth = Auth(app, db)
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+app = flask.Flask(__name__)
 
 @app.route("/")
-def base():
-    return flask.render_template("base.html")
+def start():
+    return flask.render_template("start.html")
+
+@app.route("/")
+def player():
+    return flask.render.template("player.html")
+app.run()
