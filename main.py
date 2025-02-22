260deff1734a08fee993e266c812a31560e4bde2
Lavl <148311044+LolyAlchemist@users.noreply.github.com>
2025-02-22 20:56:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,5 +10,16 @@ def start():
 def player():
     return flask.render_template("player.html")
 
+@app.route("/sales")
+def sales():
+    return flask.render_template("sales.html")
+
+@app.route("/voice")
+def voice():
+    return flask.render_template("voice.html")
+
+@app.route("/stars")
+def stars():
+    return flask.render_template("stars.html")
 
 app.run(debug=True)
