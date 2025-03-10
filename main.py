0b5b736d1e60483b4829404dc9c5a7f8157f692d
Lavl <148311044+LolyAlchemist@users.noreply.github.com>
2025-03-10 23:58:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,29 @@ def player():
 
 @app.route("/sales")
 def sales():
+    data1 = {
+    "Version": ["1.0", "01.janv", "01.febr", "01.marts", "01.apr", "01.maijs"],
+    "Release Date": ["July 4, 2024", "15.aug.24", "September 26, 2024", "November 7, 2024", "December 18, 2024", "January 22, 2025"],
+    "Active Player Count": ["50 million", "Not specified", "Not specified", "Not specified", "Not specified", "Not specified"],
+    "Revenue": ["$50M in 11 days; $100M in first month", "$32.5M in August 2024; $34.5M in September 2024", "Not specified", "Not specified", "$8.6M daily; $250M total", "Not specified"],
+    "In-Game Purchases": ["Initial in-game purchase offerings", "Special banners led to increased purchases", "New in-game items and features", "Additional in-game content and purchase options", 
+                          "New characters, banners, and events introduced", "New outfits and Signal Searches introduced"]
+}
+
+    df3 = pd.DataFrame(data1)
+
+    revenue_values = [50, 32.5, 0, 0, 250, 0]
+    df3['Revenue Values'] = revenue_values
+
+
+    df3.plot(y='Revenue Values', kind='pie', labels=df3['Version'], startangle=90, autopct='%1.1f%%', figsize=(8, 8))
+
+    plt.title('Ieņēmumu sadalījums pēc versijas')
+    plt.ylabel('') 
+    plt.savefig('sales_genshin.png')
+
     return flask.render_template("sales.html")
+
 
 @app.route("/voice")
 def voice():
